b3afc8c1da42dc863ab8cc99558583232c248372
Connor Bryan <cchromium@gmail.com>
2017-07-21 22:33:30 -0500

    Added CRA

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Merkelle from './Merkelle';
+import './App.css';
+
+class App extends Component {
+  render = () => <Merkelle />
+}
+
+export default App;
---
+++ client/src/Merkelle/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Merkelle extends Component {
+  render() {
+    return (
+      <div>
+        Merkelle
+      </div>
+    );
+  }
+}
