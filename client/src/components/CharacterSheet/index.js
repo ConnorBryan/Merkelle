9706782aaa8179cf19fe4440bc6d12801bfb78b3
Connor Bryan <cchromium@gmail.com>
2017-07-24 17:04:15 -0500

    Added elements for HP, EXP

--- client/src/components/CharacterSheet/index.js
+++ client/src/components/CharacterSheet/index.js
@@ -1,6 +1,79 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Image, Icon, Item, Progress } from 'semantic-ui-react';
 import AbilityScores from '../AbilityScores';
+
+const AdventurerInfo = ({ name, race, _class }) => (
+  <Item.Group>
+    <Item>
+      <Item.Image
+        src='http://placehold.it/200x200'
+        shape='circular'
+        size='mini' />
+      <Item.Content>
+        <Item.Header>
+          {name}
+        </Item.Header>
+        <Item.Description>
+          {race} {_class}
+        </Item.Description>
+      </Item.Content>
+    </Item>
+  </Item.Group>
+);
+
+const AdventurerHPandEXP = ({ currentHitpoints, hitpointMaximum, level, experiencePoints }) => {
+  const experiencePointsToLevel = {
+    1: 0,
+    2: 300,
+    3: 900,
+    4: 2700,
+    5: 6500,
+    6: 14000,
+    7: 23000,
+    8: 34000,
+    9: 48000,
+    10: 64000,
+    11: 85000,
+    12: 100000,
+    13: 120000,
+    14: 140000,
+    15: 165000,
+    16: 195000,
+    17: 225000,
+    18: 265000,
+    19: 305000,
+    20: 355000,
+  };
+
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={2}>
+          <Icon name='heartbeat' />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Progress
+            percent={(currentHitpoints / hitpointMaximum) * 100}
+            color='red'
+            progress
+            inverted />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={2}>
+          <Icon name='trophy' />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Progress
+            percent={(experiencePoints / experiencePointsToLevel[level + 1]) * 100}
+            color='orange'
+            progress
+            inverted />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
 
 export default ({ adventurer }) => {
   return (
@@ -8,7 +81,10 @@ export default ({ adventurer }) => {
       <Grid.Row>
 
         <Grid.Column width={6}>
-          Name, Race, Class
+          <AdventurerInfo
+            name={adventurer.name}
+            race={adventurer.race}
+            _class={adventurer.class} />
         </Grid.Column>
 
         <Grid.Column width={6}>
@@ -20,7 +96,11 @@ export default ({ adventurer }) => {
       <Grid.Row>
 
         <Grid.Column width={5}>
-          HP, EXP
+          <AdventurerHPandEXP
+            currentHitpoints={adventurer.currentHitpoints}
+            hitpointMaximum={adventurer.hitpointMaximum}
+            level={adventurer.level}
+            experiencePoints={adventurer.experiencePoints} />
         </Grid.Column>
 
         <Grid.Column width={5}>
