4d2cec7c6f3bfdc081bc53473e3791d689443d67
Connor Bryan <cchromium@gmail.com>
2017-07-24 15:48:39 -0500

    Added Alignment component

---
+++ client/src/components/AbilityScores/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Table, Segment } from 'semantic-ui-react';
+
+export default ({ abilityScores }) => (
+  <Segment compact>
+    <Table
+      basic='very'
+      celled
+      collapsing>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>
+            Ability Score
+          </Table.HeaderCell>
+          <Table.HeaderCell>
+            Value
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {Object.keys(abilityScores).map((abilityScore, i) => (
+          <Table.Row key={i}>
+            <Table.Cell>
+              {abilityScore}
+            </Table.Cell>
+            <Table.Cell>
+              {abilityScores[abilityScore]}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  </Segment>
+);
--- client/src/components/AdventurersView/index.js
+++ client/src/components/AdventurersView/index.js
@@ -1,45 +1,17 @@
 import React from 'react';
-
-import { Table, Segment } from 'semantic-ui-react';
-
-const AbilityScores = ({ abilityScores }) => (
-  <Segment compact>
-    <Table
-      basic='very'
-      celled
-      collapsing>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>
-            Ability Score
-          </Table.HeaderCell>
-          <Table.HeaderCell>
-            Value
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {Object.keys(abilityScores).map((abilityScore, i) => (
-          <Table.Row key={i}>
-            <Table.Cell>
-              {abilityScore}
-            </Table.Cell>
-            <Table.Cell>
-              {abilityScores[abilityScore]}
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  </Segment>
-);
+import AbilityScores from '../AbilityScores';
+import Alignment from '../Alignment';
 
 export default ({ blockchain }) => {
   if (blockchain.length > 0) {
     const mostRecentBlock = blockchain[blockchain.length - 1];
     const adventurersById = (JSON.parse(mostRecentBlock.data)).entitiesById.filter(entity => entity.type === 'ADVENTURER');
+    const adventurer = adventurersById[0];
     return (
-      <AbilityScores abilityScores={adventurersById[0].abilityScores} />
+      <div>
+        <AbilityScores abilityScores={adventurer.abilityScores} />
+        <Alignment alignment={adventurer.alignment} />
+      </div>
     );
   }
   return <p>Loading...</p>
